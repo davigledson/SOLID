97eeafe97c08b43da6cfb083e20413ff4c0a151f
Davi Gledson <davigledson@outlook.com>
2026-02-11 04:59:56 -0300

    classes padrões

--- app_carrinho_compras/index.php
+++ app_carrinho_compras/index.php
@@ -4,4 +4,23 @@ require __DIR__ . "/vendor/autoload.php";
 
 use App\CarrinhoComprar;
 
-echo CarrinhoComprar::teste();
+
+$carrinho1 = new CarrinhoComprar();
+echo 'Valor Total ' . $carrinho1->exibirValorTotal();
+echo '<br/>';
+
+$carrinho1->adicionarItem('Bike', 750);
+$carrinho1->adicionarItem('gato', 150);
+$carrinho1->adicionarItem('cachorro', 70);
+
+print_r(value: $carrinho1->exibirItens());
+echo '<br/>';
+
+echo 'Valor Total ' . $carrinho1->exibirValorTotal();
+echo '<br/>';
+
+echo 'Status ' . $carrinho1->exibirStatus();
+echo '<br/>';
+$carrinho1->confirmarPedido();
+echo 'Status ' . $carrinho1->exibirStatus();
+echo '<br/>';
--- app_carrinho_compras/src/CarrinhoComprar.php
+++ app_carrinho_compras/src/CarrinhoComprar.php
@@ -4,8 +4,68 @@ namespace App;
 
 class CarrinhoComprar {
 
+
+    //atributos
+    private $itens;
+    private $status;
+
+    private $valorTotal;
+
+
+    //metodos
+
+public function __construct() {
+    $this->itens = [];
+     $this->status = 'aberto';
+      $this->valorTotal = 0;
+
+}
  public static function teste(){
 
  return "OK";
+ }
+
+ public  function exibirItens(){
+
+ return $this->itens;
+ }
+
+ public function adicionarItem(string $item, float $valor){
+    array_push($this->itens,[
+    "item"=> $item,
+    "valor"=> $valor
+
+    ]);
+    $this->valorTotal += $valor;
+    return true;
+    
+ }
+
+ public function exibirValorTotal(){
+    return $this->valorTotal;
+ }
+
+ public function exibirStatus(){
+    return $this->status;
+ }
+
+ public function confirmarPedido(){
+
+ if ($this->validarCarrinho()){
+    $this->status = 'Confirmado';
+    $this->enviarEmailConfirmacao();
+
+    return true;
+ }
+    return false;
+ }
+
+  public function enviarEmailConfirmacao(){
+    $this->status = '<br/> ... enviar email';
+ }
+
+ 
+  public function validarCarrinho(){
+    return count($this->itens) > 0;
  }
 }
